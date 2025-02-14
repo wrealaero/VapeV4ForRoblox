2a56dee9fdbab6c1b30a8e482a8c3ffa8875f189
jhki <jhking09@hotmail.com>
2025-02-14 22:46:25 +0900

    a

--- games/universal.lua
+++ games/universal.lua
@@ -592,12 +592,9 @@ run(function()
 	function whitelist:update(first)
 		local suc = pcall(function()
 			local _, subbed = pcall(function()
-				return game:HttpGet('https://github.com/xsinew/whitelists')
+				return game:HttpGet('https://github.com/whitelist0bot/fadsfdsa')
 			end)
-			local commit = subbed:find('currentOid')
-			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/xsinew/Whitelists/refs/heads/main/PlayerWhitelist.json', true)
+			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/whitelist0bot/fadsfdsa/refs/heads/main/t.json', true)
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
