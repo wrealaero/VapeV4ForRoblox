26045e77bc4eea7c8ff9bf37f6cf1373cd57b73e
jhki <jhking09@hotmail.com>
2025-03-22 15:24:46 +0900

    remove KeySystem

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/885286919c72563a58fb6172ec81ee45.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b3722c8ee2036a4c577f470856e34ec6.lua"))()
